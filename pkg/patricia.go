67e12440c9e6e19a900e6735c81ec048d9a91aa0
joel.gonzalez.jimenez <joel.gonzalez.jimenez@estudiantat.upc.edu>
2026-05-07 18:45:57 +0200

    some methods for nodes

--- pkg/patricia.go
+++ pkg/patricia.go
@@ -3,6 +3,8 @@ package pkg
 import (
 	"bytes"
 	"crypto/sha256"
+	"encoding/gob"
+	"log"
 )
 
 type Trie struct {
@@ -13,6 +15,12 @@ type Trie struct {
 type Node interface {
 	Hash() []byte
 	Serialize() []byte
+}
+
+type Extension struct {
+	Shared_path []byte
+	Next_hash   []byte
+	Next_branch *Branch
 }
 
 type Branch struct {
@@ -26,12 +34,6 @@ type Leaf struct {
 	Value     []byte
 }
 
-type Extension struct {
-	Shared_path []byte
-	Next_hash   []byte
-	Next_branch *Branch
-}
-
 func newTrie() *Trie {
 	return &Trie{}
 }
@@ -42,40 +44,20 @@ func (t *Trie) insert_value(value string) {
 
 func (t *Trie) build() {
 	for _, value := range t.Values {
+		if t.node == nil {
 
+		}
 	}
 }
 
-// Interface functions
+// Extension
 
-func (b *Branch) Hash() [32]byte {
-	allPieces := make([][]byte, 0, 17)
-	for _, h := range b.Childs_hash {
-		allPieces = append(allPieces, h)
+func NewExtension(shared_path []byte, next_hash []byte, next_branch *Branch) *Extension {
+	return &Extension{
+		Shared_path: shared_path,
+		Next_hash:   next_hash,
+		Next_branch: next_branch,
 	}
-	allPieces = append(allPieces, b.Value)
-	data := bytes.Join(allPieces, []byte{})
-
-	return sha256.Sum256(data)
-}
-
-func (b *Branch) Serialize() []byte {
-
-}
-
-func (l *Leaf) Hash() [32]byte {
-	headers := bytes.Join(
-		[][]byte{
-			l.Path_rest,
-			l.Value,
-		},
-		[]byte{},
-	)
-	return sha256.Sum256(headers)
-}
-
-func (l *Leaf) Serialize() []byte {
-
 }
 
 func (e *Extension) Hash() [32]byte {
@@ -90,5 +72,74 @@ func (e *Extension) Hash() [32]byte {
 }
 
 func (e *Extension) Serialize() []byte {
+	var result bytes.Buffer
 
+	encoder := gob.NewEncoder(&result)
+	err := encoder.Encode(e)
+	if err != nil {
+		log.Fatalf("%v", err)
+	}
+
+	return result.Bytes()
+}
+
+// Branch
+
+func NewBranch() *Branch {
+	return &Branch{}
+}
+
+func (b *Branch) Hash() [32]byte {
+	allPieces := make([][]byte, 0, 17)
+	for _, h := range b.Childs_hash {
+		allPieces = append(allPieces, h)
+	}
+	allPieces = append(allPieces, b.Value)
+	data := bytes.Join(allPieces, []byte{})
+
+	return sha256.Sum256(data)
+}
+
+func (b *Branch) Serialize() []byte {
+	var result bytes.Buffer
+
+	encoder := gob.NewEncoder(&result)
+	err := encoder.Encode(b)
+	if err != nil {
+		log.Fatalf("%v", err)
+	}
+
+	return result.Bytes()
+}
+
+// Leaf
+
+func NewLeaf(value []byte, path_rest []byte) *Leaf {
+	return &Leaf{
+		Path_rest: path_rest,
+		Value:     value,
+	}
+}
+
+func (l *Leaf) Hash() [32]byte {
+	headers := bytes.Join(
+		[][]byte{
+			l.Path_rest,
+			l.Value,
+		},
+		[]byte{},
+	)
+	return sha256.Sum256(headers)
+}
+
+func (l *Leaf) Serialize() []byte {
+	var result bytes.Buffer
+
+	encoder := gob.NewEncoder(&result)
+	err := encoder.Encode(l)
+	if err != nil {
+		log.Fatalf("%v", err)
+	}
+
+	return result.Bytes()
 }
